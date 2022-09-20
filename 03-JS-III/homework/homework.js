e1ae87bfba75b696bc32cfd0545fff67dc29ba90
CAVIVANCOR <sistemas.cavr@gmail.com>
2022-09-19 20:55:33 -0500

    HomeWork 3 Listo

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,53 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acumulador = 0
+    for (let i = 1 ; i <= 10 ; i++){
+      acumulador = acumulador + i;
+    }
+    return acumulador;
 }
+
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var arrayPares = [];
+  for (let i = 0 ; i < array.length ; i++){
+    if ((array[i] % 2) === 0){
+      arrayPares.push(array[i]);
+    }
+  }
+  return arrayPares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var doble = array.map(function (elemento) {
+    return elemento * elemento;
+  });
+  return doble;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var acumulador = 0
+  for(elemento of array){
+    acumulador = acumulador + elemento;
+  }
+  return acumulador;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numString = num.toString();
+  return numString.length;
 }
   
   
